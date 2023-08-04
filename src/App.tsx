3c31a05a99416ad545e0d5486e9aa69796e2c37c
joseph742 <umoru.j@gmail.com>
2023-08-04 10:41:31 +0200

    list in react chapter completion

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const list = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walks",
+      numberOfComments: 3,
+      points: 4,
+      objectId: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      numberOfComments: 2,
+      points: 5,
+      objectId: 1
+    }
+  ]
+
+  const titleList = list.map(books => books.title + " ");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello world!</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+
+      <ul>
+        {
+          list.map(item => {
+            return (
+              <li key={item.objectId}>
+
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.numberOfComments}</span>
+                <span>{item.points}</span>
+              </li>
+            )
+          }
+          )
+        }
+      </ul>
+    </div>
   )
 }
 
