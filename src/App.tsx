c575c025f39e09167d2dc4b6cf6bebafc4202b4f
joseph742 <umoru.j@gmail.com>
2023-08-07 10:26:44 +0200

    React useState and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react'
 
-function App() {
-  const list = [
+type Story = {
+  title: string;
+  url: string;
+  author: string;
+  numberOfComments: number;
+  points: number;
+  objectId: number;
+}
+
+type Stories = Story[]
+
+const useStorageState = (key: string, initialState: string): [string, (newValue: string) => void] => {
+  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value)
+  },
+    [value, key]
+  )
+
+  return [value, setValue]
+}
+
+const App = () => {
+
+  const stories = [
     {
       title: "React",
       url: "https://reactjs.org/",
@@ -20,35 +44,80 @@ function App() {
     }
   ]
 
-  const titleList = list.map(books => books.title + " ");
+  const [searchTerm, setSearchTerm] = useStorageState('key','React');
+
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  }
+
+  let updatedStories = stories.filter(
+    (item) => item.title.toLocaleLowerCase().includes(
+      searchTerm.toLocaleLowerCase()
+    )
+  )
 
   return (
     <div>
       <h1>Hello world!</h1>
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
 
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+      <hr />
 
-      <ul>
-        {
-          list.map(item => {
-            return (
-              <li key={item.objectId}>
-
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.numberOfComments}</span>
-                <span>{item.points}</span>
-              </li>
-            )
-          }
-          )
-        }
-      </ul>
+      <List list={updatedStories} />
     </div>
   )
 }
+
+type SearchProps = {
+  searchTerm: string,
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Search: React.FC<SearchProps> = ({ searchTerm, onSearch }) => {
+
+  const handleEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch(event);
+  }
+
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' value={searchTerm} onChange={handleEvent} />
+    </div>
+  )
+}
+
+const List: React.FC<{ list: Stories }> = ({ list }) => {
+  return (
+
+    <ul>
+      {
+        list.map(item => {
+          return (
+            <Item key={item.objectId} {...item} />
+          )
+        }
+        )
+      }
+    </ul>
+  )
+}
+
+const Item: React.FC<Story> = ({ title, url, author, numberOfComments, points, objectId }) => {
+  return (
+    <li key={objectId}>
+
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{numberOfComments}</span>
+      <span>{points}</span>
+    </li>
+  )
+}
+
+
 
 export default App
